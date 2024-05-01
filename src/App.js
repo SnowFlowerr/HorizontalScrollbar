b2adde4bda513e15d532e177a1b92815a8ff8d84
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-01 17:19:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [scr,setScr]=useState(0)
+  let array=[];
+  for(let i=1;i<31;i++){
+    if(i<10){
+      array.push("0"+i)
+    }
+    else{
+      array.push(i);
+    }
+  }
+  const amount=170;
+  function handleLeft(e){
+    e.preventDefault();
+    if(scr>0){
+      let scroll=scr-amount;
+      if(scroll<0){
+        scroll=0;
+      }
+      setScr(scroll);
+      document.getElementById('box').scrollLeft=scroll;
+    }
+  }
+  function handleRight(e){
+    e.preventDefault();
+    if(scr<6500){
+      let scroll=scr+amount;
+      setScr(scroll);
+      document.getElementById('box').scrollLeft=scroll;
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="bigbox" id="box">
+      {array.map((element)=><div className='b' id='b'><div className="box">{element}</div></div>)}
     </div>
+    <div className='btns'>
+      <button onClick={handleLeft}>Left</button>
+      <button onClick={handleRight}>Right</button>
+    </div>
+    </>
   );
 }
 
