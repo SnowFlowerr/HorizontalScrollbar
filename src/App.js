cc1d2cd4ff5cbc51fecfac479e84cdf9a08b0c90
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-02 12:55:21 +0530

    improved

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import './App.css';
 
 function App() {
-  let array=[];
-  for(let i=1;i<31;i++){
-    if(i<10){
-      array.push("0"+i)
+  let array = [];
+  for (let i = 1; i < 31; i++) {
+    if (i < 10) {
+      array.push("0" + i)
     }
-    else{
+    else {
       array.push(i);
     }
   }
-  const amount=170.4;
-  function handleLeft(e){
+  const amount = 170.4;
+  function handleLeft(e) {
     e.preventDefault();
-      document.getElementById('bigbox').scrollLeft-=amount;
+    document.getElementById('bigbox').scrollLeft -= amount;
   }
-  function handleRight(e){
+  function handleRight(e) {
     e.preventDefault();
-      document.getElementById('bigbox').scrollLeft+=amount;
+    document.getElementById('bigbox').scrollLeft += amount;
   }
   return (
     <div className='border'>
       <div className='bigg'>
-    <div className="bigbox" id="bigbox">
-      {array.map((element)=><div><div className="box">{element}</div></div>)}
-    </div>
-    </div>
-    <div className='btns'>
-      <button onClick={handleLeft}>Left</button>
-      <button onClick={handleRight}>Right</button>
-    </div>
+        <div className="bigbox" id="bigbox">
+          {array.map((element) => <div><div className="box">{element}</div></div>)}
+        </div>
+      </div>
+      <div className='btns'>
+        <button onClick={handleLeft}>Left</button>
+        <button onClick={handleRight}>Right</button>
+      </div>
     </div>
   );
 }
